949be9a6eb7f4bd73ffad08211077c4db870dd2b
Ronmi Ren <ronmi.ren@gmail.com>
2018-09-21 00:34:12 +0800

    add doccomment for Argument

--- src/Argument.php
+++ src/Argument.php
@@ -2,6 +2,10 @@
 
 namespace Fruit\CompileKit;
 
+/**
+ * Argument represents the definition of a PHP variable, especially for function
+ * arguments.
+ */
 class Argument
 {
     private $name;
@@ -18,35 +22,53 @@ class Argument
         $this->defaultValue = new Value;
     }
 
+    /**
+     * Set type-hinting. Pass empty string to disable type-hinting.
+     */
     public function type(string $type): Argument
     {
         $this->typeHint = $type;
         return $this;
     }
 
+    /**
+     * Set default value.
+     */
     public function defaults(): Value
     {
         return $this->defaultValue;
     }
 
+    /**
+     * Helper for Argument::defaults.
+     */
     public function rawDefault(string $val): Argument
     {
         $this->defaults()->raw($val);
         return $this;
     }
 
+    /**
+     * Helper for Argument::defaults.
+     */
     public function bindDefault($val): Argument
     {
         $this->defaults()->bind($val);
         return $this;
     }
 
+    /**
+     * Helper for Argument::defaults.
+     */
     public function setDefault($val): Argument
     {
         $this->defaults()->set($val);
         return $this;
     }
 
+    /**
+     * @see Renderable
+     */
     public function render(bool $pretty = false, int $indent = 0): string
     {
         $padding = '';
